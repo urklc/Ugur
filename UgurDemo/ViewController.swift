fefa89b79b86ff56fa7913650bfdb91c2035167a
urklc <ugurkilic@live.com>
2020-04-19 11:57:41 +0300

    Add `UgurDemo` target

---
+++ UgurDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UgurDemo
+//
+//  Created by Ugur Kilic on 19/04/2020.
+//  Copyright © 2020 urklc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
